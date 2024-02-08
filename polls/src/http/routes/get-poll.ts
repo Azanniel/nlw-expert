468718fa5da051d4f9fe5c79f9fe9af17801283e
azanniel <leo.azannielttt@gmail.com>
2024-02-08 11:20:55 -0400

    feat: add websocket and pub sub pattern

--- polls/src/http/routes/get-poll.ts
+++ polls/src/http/routes/get-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import z from 'zod'
 
 import { prisma } from '../../lib/prisma'
+import { redis } from '../../lib/redis'
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -29,6 +30,33 @@ export async function getPoll(app: FastifyInstance) {
       return reply.status(404).send({ message: 'Poll not found.' })
     }
 
-    return reply.status(200).send({ poll })
+    const results = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const votes = results.reduce(
+      (obj, line, index) => {
+        if (index % 2 === 0) {
+          const score = results[index + 1]
+
+          Object.assign(obj, { [line]: Number(score) })
+        }
+
+        return obj
+      },
+      {} as Record<string, number>,
+    )
+
+    return reply.status(200).send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: option.id in votes ? votes[option.id] : 0,
+          }
+        }),
+      },
+    })
   })
 }
